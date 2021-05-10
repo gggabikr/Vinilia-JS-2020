69d4be1594c6a8fec3aab8dd70c21500622d808b
gggabikr <gggabikr@gmail.com>
2021-05-10 11:17:32 -0700

    Start of Momentum Project

---
+++ greeting.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input")
+
+/*
+여기서 querySelectorAll을 써도 되지만, 그경우는 결과값을 
+Array(list)로 가져옴. 심지어는 해당되는 값이 하나뿐일때도 
+array로 가져옴. 그럼 또 그 결과에서 하나의 값을 꺼내는 작업을 해야함.
+그게 더 일이 많아져서 그냥 이렇게 쓰는것. 
+*/
+
+function init(){
+
+}
+
+init();
