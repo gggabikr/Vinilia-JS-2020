4a7973d3442fafb4d8d1505418ea8e5426644aac
gggabikr <gggabikr@gmail.com>
2021-05-16 08:49:38 -0700

    Bg image added
    random background img from 8 imgs in selected folder.

---
+++ bg.js
@@ -0,0 +1,33 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 8
+
+
+function handleImgLoad(){
+    console.log("finished loading")
+}
+
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src=`images/${imgNumber}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+    image.addEventListener("load", handleImgLoad)
+}
+
+
+
+function genRandom(){
+    const number = Math.ceil(Math.random()*IMG_NUMBER);
+    return number;
+}
+
+
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
